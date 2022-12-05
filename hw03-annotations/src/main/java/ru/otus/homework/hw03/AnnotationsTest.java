67923374c0e68a2279fa1ab736c5729cf58a9bd9
Alexey Andreev <alexeiaandreev@gmail.com>
2022-12-05 16:29:32 +0600

    hw03-annotations: task classes

---
+++ hw03-annotations/src/main/java/ru/otus/homework/hw03/AnnotationsTest.java
@@ -0,0 +1,59 @@
+package ru.otus.homework.hw03;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnnotationsTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationsTest.class);
+
+    private int a;
+    private int b;
+
+    @Before
+    public void setUpA(){
+        a = 2;
+        logger.info("------Set up. Start a value: {}", a);
+    }
+
+    @Before
+    public void setUpB(){
+        b = 3;
+        logger.info("------Set up. Start b value: {}", b);
+    }
+
+    @After
+    public void tearDownA(){
+        a = -1;
+        logger.info("------Teardown. a end value: {}", a);
+    }
+
+    @After
+    public void tearDownB(){
+        b = -1;
+        logger.info("------Teardown. b end value: {}\n", b);
+    }
+
+    @Test
+    public void testNotEqual(){
+        logger.info("------Starting test: testNotEqual");
+        assertThat(a).isNotEqualTo(b);
+        logger.info("------Result - OK");
+    }
+
+
+    @Test
+    public void testEqual(){
+        logger.info("------Starting test: testEqual");
+        assertThat(a).isEqualTo(b);
+        logger.info("------Result - OK");
+    }
+
+    @Test
+    public void testException(){
+        logger.info("------Starting test: testException");
+        throw new NullPointerException("NPE test");
+    }
+}
---
+++ hw03-annotations/src/main/java/ru/otus/homework/hw03/TestRunner.java
@@ -0,0 +1,8 @@
+package ru.otus.homework.hw03;
+
+public class TestRunner {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        TestAnnotationUtils.runTests("ru.otus.homework.hw03.AnnotationsTest");
+    }
+}
