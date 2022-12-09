0ce54aa53c8107b6510dfba93a72e3624f213456
Alexey Andreev <alexeiaandreev@gmail.com>
2022-12-09 16:08:05 +0600

    hw08-serialization: review fix

--- hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -3,17 +3,15 @@ package ru.otus.dataprocessor;
 import com.google.gson.Gson;
 import ru.otus.model.Measurement;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
-
-    private static final String ROOT_RESOURCES_PATH = "src/test/resources";
 
     private final String fileName;
     public ResourcesFileLoader(String fileName) {
@@ -22,7 +20,8 @@ public class ResourcesFileLoader implements Loader {
 
     @Override
     public List<Measurement> load() {
-        try (Reader reader = Files.newBufferedReader(Paths.get(ROOT_RESOURCES_PATH, fileName))) {
+        try (var inputStreamReader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream(fileName))) {
+            Reader reader = new BufferedReader(inputStreamReader);
             Gson gson = new Gson();
             Measurement[] measures = gson.fromJson(reader, Measurement[].class);
             return new ArrayList<>(Arrays.asList(measures));
